842bd2df480dee452affe94c06ea9ed3ba754c6e
root <webmaster@mitur.gob.do>
2026-09-18 09:29:15 -0400

    ADDING OCA

--- account_internal_transfer/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_payment
-from . import account_bank_statement_line
---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-account-analytic",
+    description="Meta package for oca-account-analytic Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_analytic_parent>=15.0dev,<15.1dev',
+        'odoo-addon-account_analytic_required>=15.0dev,<15.1dev',
+        'odoo-addon-account_analytic_sequence>=15.0dev,<15.1dev',
+        'odoo-addon-account_analytic_tag_default>=15.0dev,<15.1dev',
+        'odoo-addon-account_move_update_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-analytic_base_department>=15.0dev,<15.1dev',
+        'odoo-addon-analytic_partner>=15.0dev,<15.1dev',
+        'odoo-addon-analytic_tag_dimension>=15.0dev,<15.1dev',
+        'odoo-addon-mrp_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-partner_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-pos_analytic_by_config>=15.0dev,<15.1dev',
+        'odoo-addon-procurement_mto_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-product_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-purchase_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-purchase_request_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-purchase_stock_analytic>=15.0dev,<15.1dev',
+        'odoo-addon-stock_analytic>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
